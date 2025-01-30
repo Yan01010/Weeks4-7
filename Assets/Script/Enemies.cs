5a88a649a8dd31293eec70c2a56a3014a4753b15
OOPS\xuais <xuyans@sheridancollege.ca>
2025-01-30 15:02:24 -0500

    Health UI

--- Assets/Script/Enemies.cs
+++ Assets/Script/Enemies.cs
@@ -1,19 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     public int health = 5;
+    public TextMeshProUGUI healthText;
 
     private void OnMouseDown()
     {
         TakeDamage(1); // Reduce health by 1 when clicked
     }
 
+    void UpdateHealthUI()
+    {
+        if (healthText != null)
+        {
+            healthText.text = "HP: " + health;
+        }
+    }
+
     void TakeDamage(int damage)
     {
         health -= damage;
+        UpdateHealthUI();
 
         if (health <= 0)
         {
@@ -23,8 +34,9 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        UpdateHealthUI();
     }
+
 
     // Update is called once per frame
     void Update()
